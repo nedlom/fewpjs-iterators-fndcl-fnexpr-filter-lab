26a813c8998f826a71cee2eab455d67dde6aefb8
Nick <nelom3@gmail.com>
2021-09-10 07:39:55 -0700

    commit tree 38d82377358f0c22552fb3fd65041860b19d4a64

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, str) {
+  return drivers.filter(driver => {
+    return driver.toLowerCase() === str.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, str) {
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase().indexOf(str.toLowerCase()) === 0;
+  })
+}
+
+function matchName(drivers, str) {
+  console.log(drivers[0]);
+  return drivers.filter(driver => {
+    return driver.name == str;
+  })
+}
